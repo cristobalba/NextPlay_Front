2ff974a54b0362f3e4e71d39f941f78e487b0022
Cristobal Benavides <cbenavidesa@uc.cl>
2024-11-22 11:35:04 -0300

    gettoken no funciona al darle el scope y el post de registro dice que no encuentra el token

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,42 @@
 import './App.css'
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import useAuthToken from './Auth/useAuthToken';
+import axios from 'axios';
 
 function App() {
-  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, logout, user, isAuthenticated, getIdTokenClaims } = useAuth0();
   const navigate = useNavigate();
+  const { token, error, fetchToken } = useAuthToken();
+
+  const checkNewUser = async () => {
+
+    const claims = await getIdTokenClaims();
+    const isNewUser = claims[import.meta.env.VITE_NAMESPACE]
+    if (isNewUser) {
+      console.log('User recién registrado:');
+    }
+    if (token) {
+      console.log(token);
+      await axios.post(`${import.meta.env.VITE_API_URL}/user/register`, {
+        auth: {
+          sub: token,
+        },
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const getTokenAndCheckUser = async () => {
+        await fetchToken();
+        checkNewUser();
+      };
+
+      getTokenAndCheckUser();
+    }
+  }, [isAuthenticated, fetchToken]);
 
   return (
     <>
@@ -19,7 +51,7 @@ function App() {
           <h2>Welcome, {user?.name}</h2>
           <button type="submit"
             className="submit-button"
-            style={{ color: 'white' }} 
+            style={{ color: 'white' }}
             onClick={() => logout({ returnTo: window.location.origin })}>
             Logout
           </button>
